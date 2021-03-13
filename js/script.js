15891dfe2acfe63737ef562ade1685c0d6694088
d00d1e <lee.julianay@gmail.com>
2021-03-13 02:17:45 -0800

    add basic site structure and functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//toggle menu
 const selectElement = function (el) {
   return document.querySelector(el);
 };
@@ -7,4 +8,33 @@ let body = selectElement("body");
 
 menuToggler.addEventListener("click", () => {
   body.classList.toggle("open");
+});
+
+//scroll reveal
+ScrollReveal().reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300,
+});
+
+ScrollReveal().reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
+ScrollReveal().reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
+ScrollReveal().reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
 });
